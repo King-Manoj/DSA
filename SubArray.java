97f313bc1ffb2fce35c8c75cdf40297d7d00398f
King-Manoj <kingmano2002@gmail.com>
2024-07-30 18:46:38 +0530

    SubArray

---
+++ SubArray.java
@@ -0,0 +1,27 @@
+package dsa;
+
+import java.util.*;
+
+
+public class SubArray {
+
+	public static void main(String[] args) {
+		 int[]  a = {1,2,3,4};
+		 int n = a.length;
+		 
+		 List<List<Integer>> l = new ArrayList<>();
+		 for(int i=0;i<n;i++) {
+			 
+			 for(int j=i;j<n;j++) {
+				 List<Integer> l2 = new ArrayList<>();
+				 for(int k=i;k<=j;k++) {
+					 l2.add(a[k]);
+				 }
+				 l.add(l2);
+			 }
+			 
+		 }
+		 System.out.println(l);
+	}
+
+}
