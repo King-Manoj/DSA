cff577c32e633a8c146ecc831604a49e1a688eda
King-Manoj <kingmano2002@gmail.com>
2024-08-01 19:28:38 +0530

    Insertion Sort Added

--- InsertionSort.java
+++ InsertionSort.java
@@ -17,11 +17,9 @@ public class InsertionSort {
 		for(int i : a) {
 			System.out.print(i + " ");
 		}
-
 	}
 	public static void insertionSort(int [] a) {
 		int n = a.length;
-		
 		for(int i=1;i<n;i++) {
 			for(int j=i-1;j>0;j--) {
 				if(a[j]<a[j-1]) {
