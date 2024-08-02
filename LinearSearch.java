e134a02d2c31dcaaebf180b2cbd80145cd06bda3
King-Manoj <kingmano2002@gmail.com>
2024-08-02 12:57:31 +0530

    LinearSearch Added

---
+++ LinearSearch.java
@@ -0,0 +1,25 @@
+package dsa;
+			//Inventor - John von Neumann
+			//Time Complexity 
+				//Best Case - O(1)
+				//Worst Case - O(n)
+			//Space Complexity - O(1)
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		 int [] a = {2,4,3,1,6,8,5,9};
+		 
+		 System.out.println(linearSearch(a,8));
+	}
+	public static int linearSearch(int[] a, int target) {
+		int n = a.length;
+		
+		for(int i=0;i<n;i++) {
+			if(a[i]==target)
+				return i;
+		}
+		return -1;
+	}
+
+}
